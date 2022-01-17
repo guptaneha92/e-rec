62c6f4886dc9ab86e5027d6cd92179211a5527fd
guptaneha92 <neha.gupta16@iimranchi.ac.in>
2022-01-17 15:22:37 -0500

    Added function to extract text

--- erec/data_preprocessing/pre_processing.py
+++ erec/data_preprocessing/pre_processing.py
@@ -36,4 +36,44 @@ class DataProcessing():
             temp = pd.json_normalize(listings)
             df_list.append(temp)
         self.merged_df = pd.concat(df_list)
-        logger.info('Raw files merged')
+        logger.info('Raw files merged')    
+    
+    def extract_text(self, row:str, use_set:bool=False) -> str:
+        """Method to extract raw text from the features.
+
+        Args:
+            row (str): Raw record
+            use_set (bool, optional): Flag to extract unique values in raw record. Defaults to False.
+
+        Returns:
+            str: Formatted text
+        """
+        if use_set:
+            column_val = set()
+        else:
+            column_val = []
+        if isinstance(row, float):
+            if math.isnan(row):
+                if isinstance(column_val, set):
+                    column_val.add(np.nan)
+                else:
+                    column_val.append(np.nan)
+        elif len(row):
+            for val in row:
+                language_val = val.get('language_tag')
+                if language_val in self.language_filter:
+                    feature_val = val.get('value').lower()
+                    if feature_val not in column_val:
+                        if isinstance(column_val, set):
+                            column_val.add(feature_val)
+                        else:
+                            column_val.append(feature_val)
+        if not column_val:
+            column_val = np.nan
+        elif np.nan not in column_val and isinstance(column_val, set):
+            column_val = ' '.join(list(column_val))
+        elif np.nan not in column_val:
+            column_val = ' '.join(column_val)
+        else:
+            column_val = np.nan
+        return column_val
